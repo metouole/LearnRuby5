ce58a42c2e0909485be01848c9ffad1845404256
SOMDA Métouolé Mwinbé <yvesomda93@yahoo.fr>
2017-09-19 00:13:25 +0000

    Initialisation de mon projet apprendre ruby on rails 5

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < 	ApplicationController
+
+
+
+	def salut
+		@name= params[:name]
+	end
+
+	def home
+	end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+	root to: 'pages#home'
+
+	get '/salut(/:name)', to: 'pages#salut', as: 'salut'
+end
